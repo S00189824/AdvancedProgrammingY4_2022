9175079137b832894b28f438d1b17f98fea7cf50
hypensanez12 <46888854+hypensanez12@users.noreply.github.com>
2022-02-06 20:38:24 +0000

    s

--- AdvancedProgrammingLab1/Form1.Designer.cs
+++ AdvancedProgrammingLab1/Form1.Designer.cs
@@ -29,13 +29,140 @@ namespace AdvancedProgrammingLab1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbl_Code = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.bw_CodeBreaker = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.AccessibleName = "btn_Generate";
+            this.button1.Location = new System.Drawing.Point(69, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btn_Generate);
+            // 
+            // lbl_Code
+            // 
+            this.lbl_Code.AccessibleName = "lbl_Code";
+            this.lbl_Code.AutoSize = true;
+            this.lbl_Code.Location = new System.Drawing.Point(277, 78);
+            this.lbl_Code.Name = "lbl_Code";
+            this.lbl_Code.Size = new System.Drawing.Size(0, 13);
+            this.lbl_Code.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.AccessibleName = "btn_BreakCode";
+            this.button2.Location = new System.Drawing.Point(69, 133);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Break Code";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AccessibleName = "txt_letter1";
+            this.textBox1.Location = new System.Drawing.Point(69, 180);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.AccessibleName = "txt_letter2";
+            this.textBox2.Location = new System.Drawing.Point(69, 221);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.AccessibleName = "txt_letter3";
+            this.textBox3.Location = new System.Drawing.Point(69, 264);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.AccessibleName = "txt_letter4";
+            this.textBox4.Location = new System.Drawing.Point(69, 312);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(179, 384);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(488, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(78, 384);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Progress";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lbl_Code);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbl_Code;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+        private System.ComponentModel.BackgroundWorker bw_CodeBreaker;
     }
 }
 
--- AdvancedProgrammingLab1/Form1.cs
+++ AdvancedProgrammingLab1/Form1.cs
@@ -7,14 +7,98 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace AdvancedProgrammingLab1
 {
     public partial class Form1 : Form
     {
+        string codeToBreak;
+
         public Form1()
         {
             InitializeComponent();
+
+            //Set properties of the background worker are ore of interest to us
+            bw_CodeBreaker.WorkerReportsProgress = true;
+
+            //insert a new instance of a delegate which points to the method handling the DoWork event
+            bw_CodeBreaker.DoWork += Bw_CodeBreaker_DoWork;
+            bw_CodeBreaker.ProgressChanged += Bw_CodeBreaker_ProgressChanged;
+            bw_CodeBreaker.RunWorkerCompleted += Bw_CodeBreaker_RunWorkerCompleted;
+        }
+
+        private void Bw_CodeBreaker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            
+        }
+
+        private void Bw_CodeBreaker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            
+        }
+
+        private void Bw_CodeBreaker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //place the code behind the background worker in here
+
+            //retrieve the argument
+            string cToBreak = (string)e.Argument;
+
+            char[] solvedCode = new char[4];
+
+            for (int i = 0; i < 65535; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (cToBreak[j] == (char)i)
+                        solvedCode[j] = (char)i;
+                    Thread.Sleep(1000);
+                }
+                    
+
+            }
+
+            //display
+
+            Invoke(new MethodInvoker(delegate
+            {
+
+                textBox1.Text = solvedCode[0].ToString();
+                textBox2.Text = solvedCode[1].ToString();
+                textBox3.Text = solvedCode[2].ToString();
+                textBox4.Text = solvedCode[3].ToString();
+
+            }));
+        }
+
+        private void btn_Generate(object sender, EventArgs e)
+        {
+            //Generate a random 4 letter code from the 65535 inicode available
+
+            Random rndUNICodeLetter = new Random();
+
+            codeToBreak = string.Empty;
+
+            for (int i = 0; i < 4; i++)
+            {
+                //Generate a random unicode
+                codeToBreak += (char)(rndUNICodeLetter.Next(65535));
+            }
+
+            lbl_Code.Text = codeToBreak;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Check if a code is here to break
+            //if the code is there we are going to start the background worker which performs the brute force attack
+
+            if (codeToBreak != string.Empty)
+                //start the background worker and pass the codeToBreak as argument
+                bw_CodeBreaker.RunWorkerAsync(argument: codeToBreak);
+            else
+                MessageBox.Show("Please Generate Code First");
         }
     }
 }
